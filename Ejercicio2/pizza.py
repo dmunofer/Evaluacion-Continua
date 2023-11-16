7452a2e84f905dd0fc0fa02ea6c96ba8e234531d
dmunofer <diego.fmz@hotmail.com>
2023-11-16 01:53:32 +0100

    cambios

---
+++ Ejercicio2/pizza.py
@@ -0,0 +1,8 @@
+class Pizza:
+    def __init__(self):
+        self.tipo_masa = ""
+        self.salsa = ""
+        self.ingredientes_principales = []
+        self.tecnicas_coccion = ""
+        self.presentacion = ""
+        self.maridaje_recomendado = ""
