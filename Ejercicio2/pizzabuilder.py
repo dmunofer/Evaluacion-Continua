37c6b67092b2c381488a8b0781b85c6ce34e4416
dmunofer <diego.fmz@hotmail.com>
2023-11-16 01:52:57 +0100

    cambios

---
+++ Ejercicio2/pizzabuilder.py
@@ -0,0 +1,24 @@
+class PizzaBuilder:
+    def build_dough(self):
+        pass
+
+    def build_sauce(self):
+        pass
+
+    def build_toppings(self):
+        pass
+
+    def build_cooking_technique(self):
+        pass
+
+    def build_presentation(self):
+        pass
+
+    def build_pairing(self):
+        pass
+
+    def build_extras(self):
+        pass
+
+    def get_pizza(self) -> Pizza:
+        pass
