81df1632ce9f0ab9ff8bba91d9144c30184f5e2a
dmunofer <diego.fmz@hotmail.com>
2023-11-27 20:20:50 +0100

    main actualizado lanzador

--- Ejercicio Pizza/main.py
+++ Ejercicio Pizza/main.py
@@ -1,3 +1,4 @@
+from composite import MenuItem, MenuComposite
 from pizzadeliziosa import PizzaDeliziosoBuilder
 from director import PizzaDirector
 from CSVManager import CSVManager
@@ -18,12 +19,35 @@ def main():
     director.construir_pizza()
     pizza_personalizada = builder_delizioso.get_pizza()
 
+    # Crear elementos (pizzas, bebidas, etc.)
+    pizza_margarita = MenuItem("Pizza Margarita", 10)
+    pizza_hawaiana = MenuItem("Pizza Hawaiana", 12)
+    cocacola = MenuItem("Coca Cola", 2)
+
+    # Crear menús compuestos
+    menu_pizza = MenuComposite("Menú Pizza")
+    menu_pizza.add_component(pizza_margarita)
+    menu_pizza.add_component(pizza_hawaiana)
+
+    menu_completo = MenuComposite("Menú Completo")
+    menu_completo.add_component(menu_pizza)
+    menu_completo.add_component(cocacola)
+
+    # Calcular precios
+    price_pizza = pizza_personalizada.get_price()
+    price_menu_pizza = menu_pizza.get_price()
+    price_menu_completo = menu_completo.get_price()
+
     # Muestra los detalles de la pizza personalizada
     mostrar_detalles_pizza(pizza_personalizada)
 
+    # Muestra los precios de los elementos y menús
+    print(f"Precio Pizza Personalizada: ${price_pizza}")
+    print(f"Precio Menú Pizza: ${price_menu_pizza}")
+    print(f"Precio Menú Completo: ${price_menu_completo}")
+
     # Guarda la pizza personalizada en un archivo CSV
-    csv_writer = PizzaDirector("pizzas_personalizadas.csv")
-    csv_writer.write_pizza_to_csv(pizza_personalizada)
+    CSVManager.save_to_csv(pizza_personalizada)
 
 if __name__ == "__main__":
     main()
